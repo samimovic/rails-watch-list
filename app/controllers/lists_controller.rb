ec2eb637865a7985aea8a896bf36299993bbabec
git config --global user.email anis_samimi@hotmail.com <anis_samimi@hotmail.com>
2022-02-17 14:31:54 +0100

    actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,23 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
 end
