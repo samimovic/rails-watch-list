c7aebbc32b3ed1879c92ba91b637a876a8674fb9
git config --global user.email anis_samimi@hotmail.com <anis_samimi@hotmail.com>
2022-02-17 12:48:07 +0100

    list done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,14 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
+  resources :list, only: %i[index show new create]
 end
