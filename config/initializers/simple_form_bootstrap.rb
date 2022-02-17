97ea3aaf2db948a1c757d8a4cc690f1a643dcb0c
git config --global user.email anis_samimi@hotmail.com <anis_samimi@hotmail.com>
2022-02-17 11:31:39 +0100

    fresh setup

---
+++ config/initializers/simple_form_bootstrap.rb
@@ -0,0 +1,3 @@
+rm -rf app/assets/stylesheets
+curl -L https://github.com/lewagon/stylesheets/archive/master.zip > stylesheets.zip
+unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets
